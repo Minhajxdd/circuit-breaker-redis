05e06c53db3e8c62db68e516a067e4e90cb4904e
Mohammed Minhaj <nijulinux@gmail.com>
2025-05-25 21:37:25 +0530

    feat:(gateway): updating gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -3,10 +3,13 @@ const app = express();
 
 import ROUTES from './config/routes.js';
 import proxy from './services/proxy.js';
+import { errorHandler } from "./middlewares/error-handler.js";
 
 const port = process.env.PORT || 3000;
 
 proxy.setupProxy(app, ROUTES);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log("Gateway running on port :" + port);
